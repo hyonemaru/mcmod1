aa085a0f13289cfbe41c63116b15d1217e72571e
ICHI-\yonem <yonemaru15@gmail.com>
2023-11-19 01:05:17 +0900

    Changes

--- src/main/java/com/addfoodmod/Blocks/AddCrop01.java
+++ src/main/java/com/addfoodmod/Blocks/AddCrop01.java
@@ -1,8 +1,15 @@
 package com.addfoodmod.Blocks;
 
 import com.addfoodmod.AddFoodMod;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class AddCrop01 extends BlockCrops {
 
@@ -18,5 +25,34 @@ public class AddCrop01 extends BlockCrops {
     @Override
     protected Item getCrop() {
         return AddFoodMod.ITEM_ADD_CROP_01;
+    }
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+        super.updateTick(worldIn, pos, state, rand);
+
+
+        BlockPos down = pos.down();
+        IBlockState downState = worldIn.getBlockState(down);
+
+        if (downState.getBlock() != Blocks.FARMLAND) {
+
+            dropBlockAsItem(worldIn, pos, state, 0);
+            worldIn.setBlockToAir(pos);
+        }
+    }
+    @Override
+    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
+        super.neighborChanged(state, worldIn, pos, blockIn, fromPos);
+
+        // 下のブロックをチェック
+        BlockPos down = pos.down();
+        IBlockState downState = worldIn.getBlockState(down);
+
+        // 下のブロックが耕された土でない場合
+        if (downState.getBlock() != Blocks.FARMLAND) {
+            // 作物ブロックを破壊し、種をドロップ
+            dropBlockAsItem(worldIn, pos, state, 0);
+            worldIn.setBlockToAir(pos);
+        }
     }
 }
